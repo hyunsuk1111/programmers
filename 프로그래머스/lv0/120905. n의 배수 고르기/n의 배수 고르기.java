92b0c167b3bef2b248325e3a59a1b0bfd5938f66
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2023-01-11 14:07:42 +0900

    [level 0] Title: n의 배수 고르기, Time: 3.35 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n, int[] numlist) {
         IntStream answer = Arrays.stream(numlist).filter(v -> ((v % n) == 0));
-                        
+				
         return answer.toArray();
     }
 }
