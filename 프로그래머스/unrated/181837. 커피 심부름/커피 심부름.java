ecd55467dc3d6fa69c894ccc9c50afa5a78c7fa1
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2023-06-30 14:27:27 +0900

    [unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for(String str : order){
+            if(str.contains("latte")){
+                answer += 5000;
+            }else if(str.contains("americano") || str.contains("anything")){
+                answer += 4500;
+            }
+        }
+        
+        return answer;
+    }
+}
