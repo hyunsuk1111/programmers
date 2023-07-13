daf6c6a3f65554c95f5f0f47a1ac3db8c83f2ade
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2023-07-13 14:31:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];        
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        //기준값
+        map.put(words[0], 0);
+        for(int i = 1; i < words.length; i++){
+            map.put(words[i], 0);     
+            if(words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0) || map.size() < i + 1){
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                break;
+            }
+        }                
+
+        
+        
+        return answer;
+    }
+}
